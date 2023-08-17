034fcf0b994a8851e1aff652253ac229bc76ad34
juanhoyos06 <jphoyos0610@gmail.com>
2023-08-17 18:49:28 -0500

    feat: get data form from js

--- Actividad_2-ejemplos/index.js
+++ Actividad_2-ejemplos/index.js
@@ -1,4 +1,21 @@
 const getDataForm = () => {
-    console.log("hola");
-    alert("Hola")
+    const txtName = document.getElementById("txtName").value
+    const txtEmail = document.getElementById("txtEmail").value
+    const txtDescription = document.getElementById("txtDescription").value
+    const txtConditions = document.getElementById("txtConditions").checked
+    const activityType = document.getElementById("activityType").checked
+    const taskType = document.getElementById("taskType").checked
+    
+    // Create JSON
+    const data = {
+        name: txtName,
+        email: txtEmail,
+        description: txtDescription,
+        condition: txtConditions,
+        activityType,
+        taskType
+
+    }
+
+    console.log(txtName)
 }
